7254280fca1a98b63e330b2d09cdf49164d1a261
Tayler Mulligan <mulligantayler@gmail.com>
2013-03-27 19:20:37 -0700

    Fixed some bugs

--- chess.py
+++ chess.py
@@ -28,7 +28,6 @@ class Chess(object):
         """
         Resets board and captured pieces.
         """
-
         self.white_captured = ""
         self.black_captured = ""
 
@@ -50,6 +49,139 @@ class Chess(object):
 
         self.board = start_board
 
+    # Checks if move valid
+    def check_move(self, coords1, coords2):
+        """
+        coords1: Tuple of (int,int), both ints valid indices of board; from place
+        coords2: Tuple of (int,int), both ints valid indices of board; to place
+        
+        Checks if move is valid.
+        """
+
+        def pawn(coords1, coords2):
+            # Checks if piece in movement path
+            if coords1[0]-coords2[0] == 0:
+                x = coords1[1]
+                # <= because pawns can't capture forwards
+                while x <= coords2[1][0]:
+                    if self.board[coords1[0]][x] != self.EMPTY:
+                        return False
+                    else:
+                        x += 1
+
+            # Checks if pawn moves along x axis
+            if coords1[0]-coords2[0] != 0:
+                return False
+
+            if self.board[coords2[0]][coords2[1]] != self.EMPTY:
+                if not abs(coords1[0]-coords2[0]) < 2 \
+                        or not abs(coords1[1]-coords[2]) < 2:
+                    return False 
+
+            # Checks if pawn can move 2 squares
+            elif coords1[0] == 1 or coords1[0] == 6 and \
+                    coords1 not in self.moved_pawns:
+                if not abs(coords1[0]-coords2[0]) < 3:
+                    return False
+
+            # Checks if pawn moves less than 2 squares
+            else:
+                if not abs(coords1[0]-coords2[0]) < 2:
+                    return False
+
+            return True
+
+        def rook(coords1, coords2):
+            # Checks if path is clear depending on move direction
+            # x-axis path
+            if coords1[0]-coords2[0] == 0:
+                x = coords1[1]+1
+                while x < coords2[1]:
+                    if self.board[coords1[0]][x] != self.EMPTY:
+                        return False
+                    else:
+                        x += 1
+            # y-axis path
+            elif coords1[1]-coords2[1] == 0:
+                x = coords1[0]+1
+                while x < coords2[0]:
+                    if self.board[coords1[1]][x] != self.EMPTY:
+                        return False
+                    else:
+                        x += 1
+
+            # Ensures move is only along one axis
+            x_delta = coords1[0]-coords2[0]
+            y_delta = coords1[1]-coords2[1]
+            if coords1[0]-coords[2] != 0 and coords1[1]-coords2[1] != 0:
+                print "not only one direction"
+                return False
+            
+            return True
+
+        def knight(coords1, coords2):
+            if not abs(coords1[0]-coords2[0]) == 1 \
+                    and not abs(coords1[1]-coords2[1]) == 2 \
+                    or not abs(coords1[1]-coords2[1]) == 1 \
+                    and not abs(coords1[0]-coords2[0]) == 2:
+                return False
+
+            return True
+
+        def bishop(coords1, coords2):
+            # Checks if move is diagnol
+            if abs(coords1[0]-coords2[0]) != abs(coords1[1]-coords2[1]):
+                return False
+
+            # Checks if path is empty
+            x = coords1[0]+1
+            y = coords2[0]+1
+            while x < coords2[1] or y < coords2[1]:
+                if self.board[x][y] != self.EMPTY:
+                    return False
+                else:
+                    x += 1
+                    y += 1
+
+            return True
+
+        def queen(coords1, coords2):
+            # Checks if valid using bishop and rook
+            return bishop(coords1, coords2) and rook(coords1, coords2)
+
+        def king(coords1, coords2):
+            # Checks if move less than 2 squares
+            if abs(coords1[0]-coords2[0]) < 2 \
+                    and abs(coords2[1]-coords2[1]) < 2:
+                return False
+            # Checks if valid  using queen
+            return queen(coords1, coords2)
+
+
+        if coords2[0] > 8 or coords2[1] > 'h':
+            return False
+
+        # Remove after move is implemented
+        coords1 = (ord(coords1[0])-ord('a'), coord1[1]-1)
+        coords2 = (ord(coords2[0])-ord('a'), coord2[1]-1)
+
+        piece = self.board[coords1[0]][coords1[1]]
+
+        if piece == self.WHITE_PAWN or piece == self.BLACK_PAWN:
+            return pawn(coords1, coords2)
+        elif piece == self.WHITE_ROOK or piece == self.BLACK_ROOK:
+            return rook(coords1, coords2)
+        elif piece == self.WHITE_KNIGHT or piece == self.BLACK_KNIGHT: 
+            return knight(coords1, coords2)
+        elif piece == self.WHITE_BISHOP or piece == self.BLACK_BISHOP:
+            return bishop(coords1, coords2)
+        elif piece == self.WHITE_QUEEN or piece == self.BLACK_QUEEN:
+            return queen(coords1, coords2)
+        elif piece == self.WHITE_KING or piece == self.BLACK_KING:
+            return king(coords1, coords2) 
+        else:
+            return False
+
     # Checks if valid, then moves. Also checks for captures
     def move(self, coords1, coords2):
         """
@@ -59,160 +191,25 @@ class Chess(object):
         Moves pieces.
         """
         # sets coords to array values (eg. 1 -> 0)
-        coords1 = (coords1[0]-1,ord(coords1[1])-ord('a'))
-        coords2 = (coords2[0]-1,ord(coords2[1])-ord('a'))
-        
-        def pawn(self, coords1, coords2):
-            # TODO Implement movement
+        coords1 = (ord(coords1[0])-ord('a'), coord1[1]-1)
+        coords2 = (ord(coords2[0])-ord('a'), coord2[1]-1)
+
+        if self.check_move(coords1, coords2):
+            # TODO Implement moving and capturing
             pass
-        def rook(self, coords1, coords2):
-            # TODO Implement movement
-            pass
-        def knight(self, coords1, coords2):
-            # TODO Implement movement
-            pass
-        def bishop(self, coords1, coords2):
-            # TODO Implement movement
-            pass
-        def queen(self, coords1, coords2):
-            # TODO Implement movement
-            pass
-        def king(self, coords1, coords2):
-            # TODO Implement movement
-            pass
-
-        def check(self, coords1, coords2):
-            """
-            coords1: Tuple of (int,int), both ints valid indices of board; from place
-            coords2: Tuple of (int,int), both ints valid indices of board; to place
-            
-            Checks if move is valid.
-            """
-
-            def pawn(self, coords1, coords2):
-                # Checks if piece in movement path
-                if coords1[0]-coords2[0] == 0:
-                    x = coords1[1]
-                    # <= because pawns can't capture forwards
-                    while x <= coords2[1]0:
-                        if self.board[coords1[0]][x] != self.EMPTY:
-                            return False
-                        else:
-                            x += 1
-
-                # Checks if pawn moves along x axis
-                if coords1[0]-coords2[0] != 0:
-                    return False
-
-                if self.board[coords2[0]][coords2[1]] != self.EMPTY:
-                    if not abs(coords1[0]-coords2[0]) < 2 \
-                    or not abs(coords1[1]-coords[2]) < 2:
-                        return False 
-
-                # Checks if pawn can move 2 squares
-                elif coords1[0] == 1 or coords1[0] == 6 and \
-                coords1 not in self.moved_pawns:
-                    if not abs(coords1[0]-coords2[0]) < 3:
-                        return False
-
-                # Checks if pawn moves less than 2 squares
-                else:
-                    if not abs(coords1[0]-coords2[0]) < 2:
-                        return False
-
-                return True
-
-            def rook(self, coords1, coords2):
-                # Checks if path is empty depending on move direction
-                if coords1[0]-coords2[0] == 0:
-                    x = coords1[1]+1
-                    while x < coords2[1]0:
-                        if self.board[coords1[0]][x] != self.EMPTY:
-                            return False
-                        else:
-                            x += 1
-
-                elif coords1[1]-coords2[1] == 0:
-                    x = coords1[0]+1
-                    while x < coords2[0]:
-                        if self.board[coords1[1][x] != self.EMPTY:
-                                return False
-                        else:
-                            x += 1
-
-                # Ensures move is only along one axis
-                if coords1[0]-coords2[0] != 0 and coords1[1]-coords2[1] != 0:
-                    return False
-                
-                return True
-
-            def knight(self, coords1, coords2):
-                if not abs(coords1[0]-coords2[0]) == 1 \
-                and not abs(coords1[1]-coords2[1]) == 2 \
-                or not abs(coords1[1]-coords2[1]) == 1 \
-                and not abs(coords1[0]-coords2[0]) == 2:
-                    return False
-
-                return True
-
-            def bishop(self, coords1, coords2):
-                # Checks if move is diagnol
-                if abs(coords1[0]-coords2[0]) != abs(coords1[1]-coords2[1]):
-                    return False
-
-                # Checks if path is empty
-                x = coords1[0]+1
-                y = coords2[0]+1
-                while x < coords2[1] or y < coords2[1]:
-                    if self.board[x][y] != self.EMPTY:
-                        return False
-                    else:
-                        x += 1
-                        y += 1
-
-                return True
-
-            def queen(self, coords1, coords2):
-                # Checks if valid using bishop and rook
-                return bishop(coords1, coords2) and rook(coords1, coords2)
-
-            def king(self, coords1, coords2):
-                # Checks if move less than 2 squares
-                if abs(coords1[0]-coords2[0]) < 2 \
-                and abs(coords2[1]-coords2[1] < 2:
-                    return False
-                # Checks if valid  using queen
-                return queen(coords1, coords2)
-
-
-            if coords2[0] > 8 or coords2[1] > 'h':
-                return False
-
-            piece = self.board[coords1[0]][coords1[1]]
-
-            if piece == self.WHITE_PAWN or piece == self.BLACK_PAWN:
-                return pawn(coords1, coords2)
-            elif piece == self.WHITE_ROOK or piece == self.BLACK_ROOK:
-                return rook(coords1, coords2)
-            elif piece == self.WHITE_KNIGHT or piece == self.BLACK_KNIGHT: 
-                return knight(coords1, coords2)
-            elif piece == self.WHITE_BISHOP or piece == self.BLACK_BISHOP:
-                return bishop(coords1, coords2)
-            elif piece == self.WHITE_QUEEN or piece == self.BLACK:
-                return queen(coords1, coords2)
-            elif piece == self.WHITE_KING or piece == self.BLACK_KING:
-                return king(coords1, coords2) 
-            else:
-                return False
 
     def __str__(self):
-        j = ord('a')
         output = ''
-        output += "   1  2  3  4  5  6  7  8"
+        output += "   a  b  c  d  e  f  g  h"
 
-        for j in range(ord('a'), ord('h')+1):
-            output += "\n%s " % chr(j)
-            for i in self.board[j-ord('a')]:
-                output += str(i) 
+        for k in range(len(self.board)):
+            output += "\n%d " % (k+1)
+            for j in range(len(self.board[k])):
+                output += self.board[j][k]
+
+        #for j in range(1,9):
+        #    output += "\n%d " % j
+        #    for i in self.board[j-1]:
+        #        output += str(i) 
 
         return output
--- tests/chess_tests.py
+++ tests/chess_tests.py
@@ -1,20 +1,19 @@
 from nose.tools import *
 import chess
-
 class TestStart(object):
     def test_start(self):
         game = chess.Chess()
         game.start()
 
-        assert_equal(game.__str__(), "   1  2  3  4  5  6  7  8\n"\
-                                     "a [r][p][ ][ ][ ][ ][P][R]\n"\
-                                     "b [h][p][ ][ ][ ][ ][P][H]\n"\
-                                     "c [b][p][ ][ ][ ][ ][P][B]\n"\
-                                     "d [q][p][ ][ ][ ][ ][P][Q]\n"\
-                                     "e [k][p][ ][ ][ ][ ][P][K]\n"\
-                                     "f [b][p][ ][ ][ ][ ][P][B]\n"\
-                                     "g [h][p][ ][ ][ ][ ][P][H]\n"\
-                                     "h [r][p][ ][ ][ ][ ][P][R]") 
+        assert_equal(game.__str__(), "   a  b  c  d  e  f  g  h\n"\
+                                     "1 [r][h][b][q][k][b][h][r]\n"\
+                                     "2 [p][p][p][p][p][p][p][p]\n"\
+                                     "3 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+                                     "4 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+                                     "5 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+                                     "6 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+                                     "7 [P][P][P][P][P][P][P][P]\n"\
+                                     "8 [R][H][B][Q][K][B][H][R]") 
 
 class TestCheck(object):
     def setup(self):
@@ -22,154 +21,155 @@ class TestCheck(object):
         self.game.start()
 
     def test_check_pawn(self):
-        # Check normal moves
-        assert_equal(self.game.move.check((2,'a'), (3,'a')), True)
-        assert_equal(self.game.move.check((2,'b'), (5,'b')), False)
-        assert_equal(self.game.move.check((2,'c'), (3,'d')), False)
+        # Check normal check_moves
+        assert_equal(self.game.check_move(('a',2), ('a',3)), True)
+        assert_equal(self.game.check_move(('b',2), ('b',5)), False)
+        assert_equal(self.game.check_move(('c',2), ('d',3)), False)
         
         # Check moving 2 squares at start
-        assert_equal(self.game.move.check((2,'b'), (4,'b')), True
+        assert_equal(self.game.check_move(('b',2), ('b',4)), True)
 
-        # Check capture move
+        # Check capture check_move
         self.game.board[2][2] = '[P]'
         self.game.board[1][1] = '[P]'
-        assert_equal(self.game.move.check((2,'b'), (3,'c')), True)
+        assert_equal(self.game.check_move(('b',2), ('c',3)), True)
 
         # Check piece in path
         self.game.board[2][1] = '[P]'
         self.game.board[1][1] = '[P]'
-        assert_equal(self.game.move.check((2,'b'), (3,'b')), False)
-        assert_equal(self.game.move.check((2,'b'), (4,'b')), False)
+        assert_equal(self.game.check_move(('b',2), ('b',3)), False)
+        assert_equal(self.game.check_move(('b',2), ('b',4)), False)
 
     def test_check_rook(self):
-        # Check move along x axis
-        self.game.board[1][0] = '[ ]'
-        assert_equal(self.game.move.check((1,'a'), (4,'a')), True)
+        # Check check_move along x axis
+        self.game.board[0][1] = '[ ]'
+        assert_equal(self.game.check_move(('a',1), ('a',4)), True)
         
-        # Check move along y axis
-        self.game.board[3][0] = '[R]'
-        assert_equal(self.game.move.check((4,'a'), (4,'d')), True)
+        # Check check_move along y axis
+        self.game.board[0][3] = '[R]'
+        print self.game.board
+        assert_equal(self.game.check_move(('a',4), ('d',4)), True)
 
         # Check piece in path
-        assert_equal(self.game.move.check((1,'h'), (3,'h')), False)
+        assert_equal(self.game.check_move(('h',1), ('h',3)), False)
 
     def test_check_knight(self):
         # Check vertical L
-        assert_equal(self.game.move.check(1,'g'), (3,'f'))
+        assert_equal(self.game.check_move(('g',1), ('f',3)), True)
         
         # Check horizontal L
         self.game.board[3][3]
-        assert_equal(self.game.move.check((4,'d'), (6,'c')), True)
+        assert_equal(self.game.check_move(('d',4), ('c',6)), True)
 
         # Check invalid
-        assert_equal(self.game.move.check((4,'d'), (5,'c')), False)
+        assert_equal(self.game.check_move(('d',4), ('c',5)), False)
 
-        def test_check_bishop(self):
+    def test_check_bishop(self):
         self.game.board = [['[ ]']*8 for x in xrange(8)]
         self.game.board[0][0] = '[B]'
 
         # Test Trues
-        assert_equal(self.game.move.check((1,'a'), (2,'b')), True)
-        assert_equal(self.game.move.check((1,'a'), (3,'c')), True)
+        assert_equal(self.game.check_move(('a',1), ('b',2)), True)
+        assert_equal(self.game.check_move(('a',1), ('c',3)), True)
         
         # Test Falses
-        assert_equal(self.game.move.check((1,'a'), (2,'a')), False)
-        assert_equal(self.game.move.check((1,'a'), (3,'b')), False)
+        assert_equal(self.game.check_move(('a',1), ('a',2)), False)
+        assert_equal(self.game.check_move(('a',1), ('b',3)), False)
 
     def test_check_queen(self):
         self.game.board = [['[ ]']*8 for x in xrange(8)]
         self.game.board[0][0] = '[Q]'
 
-        # Check straight moves
-        assert_equal(self.game.move.check((1,'a'), (4,'a')), True)
-        assert_equal(self.game.move.check((1,'a'), (1,'d')), True)
-        assert_equal(self.game.move.check((1,'a'), (1,'j')), False)
-        assert_equal(self.game.move.check((1,'a'), (9,'a')), False)
+        # Check straight check_moves
+        assert_equal(self.game.check_move(('a',1), ('a',4)), True)
+        assert_equal(self.game.check_move(('a',1), ('d',1)), True)
+        assert_equal(self.game.check_move(('a',1), ('j',1)), False)
+        assert_equal(self.game.check_move(('a',1), ('a',9)), False)
 
-        # Check diagnol moves
-        assert_equal(self.game.move.check((1,'a'), (3,'c')), True)
-        assert_equal(self.game.move.check((1,'a'), (4,'d')), True)
-        assert_equal(self.game.move.check((1,'a'), (2,'e')), False)
+        # Check diagnol check_moves
+        assert_equal(self.game.check_move(('a',1), ('c',3)), True)
+        assert_equal(self.game.check_move(('a',1), ('d',4)), True)
+        assert_equal(self.game.check_move(('a',1), ('e',2)), False)
 
     def test_check_king(self):
         self.game.board = [['[ ]']*8 for x in xrange(8)]
         self.game.board[0][0] = '[K]'
 
         # Test diagnol
-        assert_equal(self.game.move.check((1,'a'), (2,'b')), True)
-        assert_equal(self.game.move.check((1,'a'), (3,'c')), False)
+        assert_equal(self.game.check_move(('a',1), ('b',2)), True)
+        assert_equal(self.game.check_move(('a',1), ('c',3)), False)
 
         # Test straight
-        assert_equal(self.game.move.check((1,'a'), (2,'a')), True)
-        assert_equal(self.game.move.check((1,'a'), (3,'a')), False)
-        assert_equal(self.game.move.check((1,'a'), (1,'c')), False)
+        assert_equal(self.game.check_move(('a',1), ('a',2)), True)
+        assert_equal(self.game.check_move(('a',1), ('a',3)), False)
+        assert_equal(self.game.check_move(('a',1), ('c',1)), False)
 
-class TestMove(object):
-    def setup(self):
-        self.game = chess.Chess()
-        self.game.start()
-        self.bad_move = "Invalid move."
-
-    def test_move_pawn(self):
-        self.game.move((2,'b'), (3,'b'))
-        assert_equal(self.game.__str__(), "   1  2  3  4  5  6  7  8\n"\
-                                          "a [r][p][ ][ ][ ][ ][P][R]\n"\
-                                          "b [h][ ][p][ ][ ][ ][P][H]\n"\
-                                          "c [b][p][ ][ ][ ][ ][P][B]\n"\
-                                          "d [q][p][ ][ ][ ][ ][P][Q]\n"\
-                                          "e [k][p][ ][ ][ ][ ][P][K]\n"\
-                                          "f [b][p][ ][ ][ ][ ][P][B]\n"\
-                                          "g [h][p][ ][ ][ ][ ][P][H]\n"\
-                                          "h [r][p][ ][ ][ ][ ][P][R]")    
-
-        self.game.move((2,'d'), (4,'d'))
-        assert_equal(self.game.__str__(), "   1  2  3  4  5  6  7  8\n"\
-                                          "a [r][p][ ][ ][ ][ ][P][R]\n"\
-                                          "b [h][ ][p][ ][ ][ ][P][H]\n"\
-                                          "c [b][p][ ][ ][ ][ ][P][B]\n"\
-                                          "d [q][ ][ ][p][ ][ ][P][Q]\n"\
-                                          "e [k][p][ ][ ][ ][ ][P][K]\n"\
-                                          "f [b][p][ ][ ][ ][ ][P][B]\n"\
-                                          "g [h][p][ ][ ][ ][ ][P][H]\n"\
-                                          "h [r][p][ ][ ][ ][ ][P][R]")    
-
-        assert_equal(self.game.move((2,'c'), (1, 'd')), self.bad_move)
-        assert_equal(self.game.move((2,'f'), (3,'g')), self.bad_move)
-
-    def test_move_rook(self):
-        self.game.board[1][0] = '[ ]'
-        self.game.move((1, 'a'), (3, 'a'))
-        assert_equal(self.game.__str__(),  "   1  2  3  4  5  6  7  8\n"\
-                                           "a [ ][ ][r][p][ ][ ][P][R]\n"\
-                                           "b [h][p][ ][ ][ ][ ][P][H]\n"\
-                                           "c [b][p][ ][ ][ ][ ][P][B]\n"\
-                                           "d [q][p][ ][ ][ ][ ][P][Q]\n"\
-                                           "e [k][p][ ][ ][ ][ ][P][K]\n"\
-                                           "f [b][p][ ][ ][ ][ ][P][B]\n"\
-                                           "g [h][p][ ][ ][ ][ ][P][H]\n"\
-                                           "h [r][p][ ][ ][ ][ ][P][R]")   
-
-        self.game.board[1][0] = '[ ]'
-        self.game.move((1,'a'), (5,'a'))
-        assert_equal(self.game.__str__(),  "   1  2  3  4  5  6  7  8\n"\
-                                           "a [ ][ ][ ][ ][r][ ][P][R]\n"\
-                                           "b [h][p][ ][ ][ ][ ][P][H]\n"\
-                                           "c [b][p][ ][ ][ ][ ][P][B]\n"\
-                                           "d [q][p][ ][ ][ ][ ][P][Q]\n"\
-                                           "e [k][p][ ][ ][ ][ ][P][K]\n"\
-                                           "f [b][p][ ][ ][ ][ ][P][B]\n"\
-                                           "g [h][p][ ][ ][ ][ ][P][H]\n"\
-                                           "h [r][p][ ][ ][ ][ ][P][R]")   
-
-        self.game.move((5,'a'), (5, 'e'))
-        assert_equal(self.game.__str__(),  "   1  2  3  4  5  6  7  8\n"\
-                                           "a [r][ ][ ][ ][ ][ ][P][R]\n"\
-                                           "b [h][p][ ][ ][ ][ ][P][H]\n"\
-                                           "c [b][p][ ][ ][ ][ ][P][B]\n"\
-                                           "d [q][p][ ][ ][ ][ ][P][Q]\n"\
-                                           "e [k][p][ ][ ][r][ ][P][K]\n"\
-                                           "f [b][p][ ][ ][ ][ ][P][B]\n"\
-                                           "g [h][p][ ][ ][ ][ ][P][H]\n"\
-                                           "h [r][p][ ][ ][ ][ ][P][R]")   
-
-        assert_equal(self.game.move((5, 'e'), (4, 'f')), self.bad_move)
+#class TestMove(object):
+#    def setup(self):
+#        self.game = chess.Chess()
+#        self.game.start()
+#        self.bad_check_move = "Invalid check_move."
+#
+#    def test_move_pawn(self):
+#        self.game.move(('b',2), ('b',3))
+#        assert_equal(self.game.__str__(), "   a  b  c  d  e  f  g  h\n"\
+#                                          "1 [r][h][b][q][k][b][h][r]\n"\
+#                                          "2 [p][ ][p][p][p][p][p][p]\n"\
+#                                          "3 [ ][p][ ][ ][ ][ ][ ][ ]\n"\
+#                                          "4 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                          "5 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                          "6 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                          "7 [P][P][P][P][P][P][P][P]\n"\
+#                                          "8 [R][H][B][Q][K][B][H][R]")    
+#
+#        self.game.move(('d',2), ('d',4))
+#        assert_equal(self.game.__str__(), "   a  b  c  d  e  f  g  h\n"\
+#                                          "1 [r][h][b][q][k][b][h][r]\n"\
+#                                          "2 [p][ ][p][ ][p][p][p][p]\n"\
+#                                          "3 [ ][p][ ][ ][ ][ ][ ][ ]\n"\
+#                                          "4 [ ][ ][ ][p][ ][ ][ ][ ]\n"\
+#                                          "5 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                          "6 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                          "7 [P][P][P][P][P][P][P][P]\n"\
+#                                          "8 [R][H][B][Q][K][B][H][R]")    
+#
+#        assert_equal(self.game.move(('c',2), ('d',1)), self.bad_move)
+#        assert_equal(self.game.move(('f',2), ('g',3)), self.bad_move)
+#
+#    def test_move_rook(self):
+#        self.game.board[0][1] = '[ ]'
+#        self.game.move(('a',1), ('a',3))
+#        assert_equal(self.game.__str__(),  "   a  b  c  d  e  f  g  h\n"\
+#                                           "1 [ ][h][b][q][k][b][h][r]\n"\
+#                                           "2 [ ][p][p][p][p][p][p][p]\n"\
+#                                           "3 [r][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "4 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "5 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "6 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "7 [P][P][P][P][P][P][P][P]\n"\
+#                                           "8 [R][H][B][Q][K][B][H][R]") 
+#
+#        self.game.board[1][0] = '[ ]'
+#        self.game.move(('a',3), ('a',5))
+#        assert_equal(self.game.__str__(),  "   a  b  c  d  e  f  g  h\n"\
+#                                           "1 [ ][h][b][q][k][b][h][r]\n"\
+#                                           "2 [ ][p][p][p][p][p][p][p]\n"\
+#                                           "3 [r][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "4 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "5 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "6 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "7 [P][P][P][P][P][P][P][P]\n"\
+#                                           "8 [R][H][B][Q][K][B][H][R]")   
+#
+#        self.game.move(('a',5), ('e',5))
+#        assert_equal(self.game.__str__(),  "   a  b  c  d  e  f  g  h\n"\
+#                                           "1 [ ][h][b][q][k][b][h][r]\n"\
+#                                           "2 [ ][p][p][p][p][p][p][p]\n"\
+#                                           "3 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "4 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "5 [ ][ ][ ][ ][r][ ][ ][ ]\n"\
+#                                           "6 [ ][ ][ ][ ][ ][ ][ ][ ]\n"\
+#                                           "7 [P][P][P][P][P][P][P][P]\n"\
+#                                           "8 [R][H][B][Q][K][B][H][R]") 
+#
+#        assert_equal(self.game.move(('e',5), ('f',4)), self.bad_move)
