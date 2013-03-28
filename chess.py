36b82eb6a668774f64c95dfacc5419bec58ce95b
Tayler Mulligan <mulligantayler@gmail.com>
2013-03-28 15:52:05 -0700

    * fixed bugs * fixed bishop path checking bug * fixed pawn moving 2 squares anywhere

--- chess.py
+++ chess.py
@@ -2,19 +2,19 @@ class Chess(object):
     # All possible values for board
     EMPTY = '[ ]' 
 
-    BLACK_PAWN = '[p]'
-    BLACK_KING = '[k]'
-    BLACK_QUEEN = '[q]'
-    BLACK_BISHOP = '[b]'
-    BLACK_KNIGHT = '[h]'
-    BLACK_ROOK = '[r]'
+    WHITE_PAWN = '[p]'
+    WHITE_KING = '[k]'
+    WHITE_QUEEN = '[q]'
+    WHITE_BISHOP = '[b]'
+    WHITE_KNIGHT = '[h]'
+    WHITE_ROOK = '[r]'
 
-    WHITE_PAWN = '[P]'
-    WHITE_KING = '[K]'
-    WHITE_QUEEN = '[Q]'
-    WHITE_BISHOP = '[B]'
-    WHITE_KNIGHT = '[H]'
-    WHITE_ROOK = '[R]' 
+    BLACK_PAWN = '[P]'
+    BLACK_KING = '[K]'
+    BLACK_QUEEN = '[Q]'
+    BLACK_BISHOP = '[B]'
+    BLACK_KNIGHT = '[H]'
+    BLACK_ROOK = '[R]' 
     
     # Coloured lists of pieces
     BLACK_PIECES = (BLACK_PAWN, BLACK_KING, BLACK_QUEEN, BLACK_BISHOP, BLACK_KNIGHT,
@@ -47,23 +47,30 @@ class Chess(object):
         self.white_captured = []
         self.black_captured = []
 
-        start_board = self.board[:]
-        for i in range(0, 8):
-            start_board[i][1], start_board[i][6] = self.WHITE_PAWN, self.BLACK_PAWN
+        def reset_board():
+            start_board = self.board[:]
+            print "Setting to empty"
+            for column in range(len(start_board)):
+                for row in range(len(start_board[column])):
+                    start_board[column][row] = self.EMPTY
+            for column in range(0, 8):
+                start_board[column][1], start_board[column][6] = self.BLACK_PAWN, self.WHITE_PAWN
 
-        start_board[0][0], start_board[7][0] = self.WHITE_ROOK, self.WHITE_ROOK
-        start_board[0][7], start_board[7][7] = self.BLACK_ROOK, self.BLACK_ROOK
+            start_board[0][0], start_board[7][0] = self.BLACK_ROOK, self.BLACK_ROOK
+            start_board[0][7], start_board[7][7] = self.WHITE_ROOK, self.WHITE_ROOK
 
-        start_board[1][0], start_board[6][0] = self.WHITE_KNIGHT, self.WHITE_KNIGHT
-        start_board[1][7], start_board[6][7] = self.BLACK_KNIGHT, self.BLACK_KNIGHT
+            start_board[1][0], start_board[6][0] = self.BLACK_KNIGHT, self.BLACK_KNIGHT
+            start_board[1][7], start_board[6][7] = self.WHITE_KNIGHT, self.WHITE_KNIGHT
 
-        start_board[2][0], start_board[5][0] = self.WHITE_BISHOP, self.WHITE_BISHOP
-        start_board[2][7], start_board[5][7] = self.BLACK_BISHOP, self.BLACK_BISHOP
+            start_board[2][0], start_board[5][0] = self.BLACK_BISHOP, self.BLACK_BISHOP
+            start_board[2][7], start_board[5][7] = self.WHITE_BISHOP, self.WHITE_BISHOP
 
-        start_board[3][0], start_board[3][7] = self.WHITE_QUEEN, self.BLACK_QUEEN
-        start_board[4][0], start_board[4][7] = self.WHITE_KING, self.BLACK_KING
+            start_board[3][0], start_board[3][7] = self.BLACK_QUEEN, self.WHITE_QUEEN
+            start_board[4][0], start_board[4][7] = self.BLACK_KING, self.WHITE_KING
 
-        self.board = start_board
+            return start_board
+
+        self.board = reset_board()
 
     # Checks if move valid
     def check_move(self, turn, coords1, coords2):
@@ -75,9 +82,9 @@ class Chess(object):
         """
         def pawn(coords1, coords2):
             # Checks if move is forwards
-            if coords2[1]-coords1[1] < 0 and piece == self.WHITE_PAWN:
+            if coords2[1]-coords1[1] > 0 and piece == self.WHITE_PAWN:
                 return False
-            if coords2[1]-coords1[1] > 0 and piece == self.BLACK_PAWN:
+            if coords2[1]-coords1[1] < 0 and piece == self.BLACK_PAWN:
                 return False
 
             # Checks if piece in movement path
@@ -100,17 +107,18 @@ class Chess(object):
 
 
             # Checks if pawn can move 2 squares
-            if abs(coords1[1]-coords2[1]) > 1 and coords1 in self.moved_pawns:
-                return False
+            if coords1[1] == 6 or coords1[1] == 1:
+                if abs(coords1[1]-coords2[1]) > 1 and coords1 in self.moved_pawns:
+                    return False
 
             # If pawn can move 2, checks if moves more than 2 
-            if coords1 not in self.moved_pawns:
-                if abs(coords1[1]-coords2[1]) > 2:
-                    return False
+                elif coords1 not in self.moved_pawns:
+                    if abs(coords1[1]-coords2[1]) > 2:
+                        return False
             # If pawn can't move 2, checks if moves more than 1
-            else:
-                if abs(coords1[1]-coords2[1]) > 1:
-                    return False
+                else:
+                    if abs(coords1[1]-coords2[1]) > 1:
+                        return False
 
             return True
 
@@ -161,8 +169,8 @@ class Chess(object):
 
             # Checks if path is empty
             x = coords1[0]+1
-            y = coords2[0]+1
-            while x < coords2[1] or y < coords2[1]:
+            y = coords1[1]+1
+            while x < coords2[1] and y < coords2[1]:
                 if self.board[x][y] != self.EMPTY:
                     print "path not empty"
                     return False
@@ -220,9 +228,9 @@ class Chess(object):
         if coords2[0] > 'h' or coords2[1] > 8:
             return invalid_move
 
-        # sets coords to array values (eg. 1 -> 0, 'a' -> 0)
-        coords1 = (ord(coords1[0])-ord('a'), coords1[1]-1)
-        coords2 = (ord(coords2[0])-ord('a'), coords2[1]-1)
+        # sets coords to array values (eg. 8 -> 0, 'a' -> 0)
+        coords1 = (ord(coords1[0])-ord('a'), 8-coords1[1])
+        coords2 = (ord(coords2[0])-ord('a'), 8-coords2[1])
 
         if self.check_move(turn, coords1, coords2):
             # if pawn, adds to moved pawn list
@@ -267,7 +275,7 @@ class Chess(object):
 
         capped = ''
         capped += "\nCapped: "
-        for x in self.white_captured:
+        for x in self.black_captured:
             if len(capped)%25 == 0:
                 capped += "\n"
             capped += x.strip("[]")
@@ -277,13 +285,13 @@ class Chess(object):
 
         output += "   a  b  c  d  e  f  g  h"
         for k in range(len(self.board)):
-            output += "\n%d " % (k+1)
+            output += "\n%d " % (8-k)
             for j in range(len(self.board[k])):
                 output += self.board[j][k]
 
         capped = ''
         capped += "\n\nCapped: "
-        for x in self.black_captured:
+        for x in self.white_captured:
             if len(capped)%25 == 0:
                 capped += "\n"
             capped += x.strip("[]")
@@ -293,19 +301,23 @@ class Chess(object):
         
         return output
 
+import time
+
 class Engine():
     def __init__(self):
         print "Welcome to a commandline interface version of chess!"
         print
         print "Player 1 is White, Player 2 is Black."
-        print "White pieces are uppercase, black pieces are lowercase."
+        print "White pieces are lowercase, black pieces are uppercase."
         print
         print "Input moves as <from> <to> (eg. a2 a3)."
         print "Type 'quit' to quit."
         print "If you need help at anytime, try 'help'!"
         print
         print "Read the README for more info!"
+        raw_input("Please press Enter to continue...")
         self.game = Chess()
+
 
     def parse_input(self, move):
         if move == "help":
@@ -320,6 +332,12 @@ class Engine():
                     coords2 = (move[1][0], int(move[1][1]))
                     return (coords1, coords2)
             
+            
+    def start(self):
+        self.game.start()
+        self.play()
+
+
     def play(self):
         stop = False
         turn = -1
@@ -350,8 +368,13 @@ class Engine():
                     print
                     print "Type 'quit' to quit"
                 elif coords == 'quit':
-                    valid = True
-                    stop = True
+                    sure = raw_input("Are you sure you would like to quit? (y/n) ")
+                    if sure == 'y':
+                        valid = True
+                        stop = True
+                    else:
+                        print "Continuing..."
+                        time.sleep(2)
 
             # Checks if anyones captured all the pieces
             if self.game.get_captured("white") >= 16:
@@ -363,19 +386,16 @@ class Engine():
                 stop = True
 
         print "Turns played: %d" % turn
-        play_again = raw_input("Would you like to play again? (y/n) ")
+        play_again = raw_input("\nWould you like to play again? (y/n) ")
         while True:
-            if play_again in 'yesYes':
+            if play_again == 'y':
                 self.start()
-            elif play_again in 'noNo':
+            elif play_again == 'n':
                 print "Thanks for playing!"
                 exit(0)
             else:
-                print "Not valid, yes or no?"
+                print "Not valid, 'y'es or 'n'o?"
         
-    def start(self):
-        self.game.start()
-        self.play()
 
 engine = Engine()
 engine.start()
